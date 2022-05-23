6159a9b85e8ac15c9e460bbfffb246ee972c8d73
Dat Nguyen <tandat198@gmail.com>
2022-05-23 23:52:05 +0700

    declare vars in go

---
+++ 2.basic-types/index.go
@@ -0,0 +1,4 @@
+// var var_name var_type
+
+var number int 
+var pi float64 = 3.14159
---
+++ 2.basic-types/short-var-declaration.go
@@ -0,0 +1,11 @@
+var empty string
+// is equal to
+test := ""
+numCars := 10
+temperature := 0.0
+var isFunny = true
+
+/* Outside of a function (in the global/package scopre), every
+statement begins with a keyword (var, func and so on) and so
+the construct := is not available
+*/
